de00db566998da1719a8658eaa2a3598b7a17c45
William Takeshi <williamtpereira@gmail.com>
2024-01-20 19:04:15 -0300

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-
 #[wasm_bindgen]
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -10,7 +9,6 @@ pub enum Cell {
     Dead = 0,
     Alive = 1,
 }
-
 
 #[wasm_bindgen]
 pub struct Universe {
@@ -56,8 +54,21 @@ impl Universe {
         }
         count
     }
-}
 
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
+}
 
 /// Public methods, exported to JavaScript.
 #[wasm_bindgen]
@@ -118,5 +129,33 @@ impl Universe {
 
     pub fn render(&self) -> String {
         self.to_string()
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
     }
 }
